f4f3543277e8dfebd1ebc4bd47d7263e60edc2be
alfaceor <alfaceor@a0a6e715-ba07-00d4-a874-b34d0e39e2a5>
2011-10-04 03:53:29 +0000

    adding the source files for the kernel and the main program

---
+++ trunk/cuda/heatequation/src/kernel_gpu.cu
@@ -0,0 +1,25 @@
+#include "kernel_gpu.cuH"
+
+/**
+Generate the new u in the next time step using blocks
+*/
+__global__ void new_time_blocks(float *u, float *u_new,float c) {
+    // XXX: Because the boundary conditions, this must started in 1
+    // u_[1], that is the reason why i added +1
+    int tid = threadIdx.x+1;  // this thread handles the data at its thread id
+	// The array is a matrix (N+1)*(N)
+	// Just chain the values of u in the interval 1,rowdim
+    if (tid < N)
+        u_new[tid] = u[tid+1]+u[tid-1]+u[tid-DIM]+u[tid+DIM]-4*u[tid];
+}
+
+/**
+Generate the new u in the next time step using threads
+*/
+
+__global__ void new_time_threads(float *u, float *u_new,float c) {
+    int tid = blockIdx.x;    // this thread handles the data at its thread id
+    if (tid < N)
+        u_new[tid] = u[tid+1]+u[tid-1]-4*u[tid] ;
+        //u_new[tid] = u[tid+1]+u[tid-1]+u[tid-dim]+u[tid+dim]-4*u[tid] ;
+}
