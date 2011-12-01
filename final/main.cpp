77588e1a8ef40089d90ffe5a1824d21fd4a9a8a2
alfaceor <alfaceor@a0a6e715-ba07-00d4-a874-b34d0e39e2a5>
2011-12-01 09:53:11 +0000

    Generalization for N particles and N-1 spring Problem: can't visualice the trajectories

--- final/main.cpp
+++ final/main.cpp
@@ -2,83 +2,78 @@
 Description: Leapfrog for a 1-D coupled harmonic oscillator
 author: Carlos Olivares
 e-mail: alfaceor@gmail.com
-creative commons
+license: creative commons
 */
 #include <stdio.h>
+#include <math.h>
+#include "pdb_utils.hpp"
 //#include <iostream>
 //using namespace std;
 
 #define DIM 3
-#define N 2  // Number of particles
-
-// Printing PDB file
-const char atom_line[]=
-  "ATOM  %5d%*1c";
-
-void print_pdb_atom_line(){
-	char line[81];
-	sprintf(line,atom_line,5,'C');
-	cout<<line<<endl;
-}
-
-void print_array(double arg[],int length) {
-	for (int i=0; i<length; i++){
-		cout<< arg[i]<<"\t";
-	}
-	cout<<endl;
-}
+#define N 4  // Number of particles
 
 int main(){
 
 	// Initialize variables
-	double r[N][DIM]= {{0,0,0},{0,10,0}};	// position
-	double v[N][DIM]= {{0,0,0},{0,0,0}};		// velocity
-	double a[N][DIM]= {{0,0,0},{0,0,0}};	// aceleration
+	double r[N][DIM]= {{0,0,0},{0,10,0},{0,15,0},{0,17.5,0}};	// position
+	double v[N][DIM]= {{0,0,0},{0,0,0},{0,0,0},{0,0,0}};		// velocity
+	double a[N][DIM]= {{0,0,0},{0,0,0},{0,0,0},{0,0,0}};	// aceleration
 	// System properties
-	double k[N-1] = {1};		// hooke constant
-	double l[N-1] = {5};	// natural length
-	double mass[N] = {1,1};
-	
-	
+	double k[N-1] = {1,1,1};		// hooke constant
+	double l[N-1] = {5,5,5};		// natural length
+	double mass[N] = {1,1,1};
+
+	// auxiliar variables
+	double xi[N-1][DIM];			// spring deformation vector
+		
 	// Integration parameters
 	int n_steps=100;	// iteration steps
-	double t_f=5;		// initial time
-	double t_i=0;		// final time
+	double t_f=5;			// initial time
+	double t_i=0;			// final time
 	double dt=(t_f - t_i)/n_steps;
 	
 	// TODO: generalize this, and make a function.
 	//---> BEGIN _Integrator leapfrog_
 	int n=1;
 	while(t_f>=t_i){
-	cout<<"MODEL\t"<< n <<"\n";
-		for(int i=0;i<N;i++){
-			if (i==0)
-				cout <<"ATOM\t"<<(i+1) << "\t"<<"CA\t"<<"MET A 1\t";
-			else
-				cout <<"ATOM\t"<<(i+1) << "\t"<<"C\t"<<"MET A 1\t";
-
+		// ---> BEGIN spring deformation
+		for(int s=0;s<N-1;s++){
+			double norm_xi;		// deformation length
 			for (int d=0;d<DIM;d++){
-				// calculate the aceleration (force) over the particle
-				
-				if(d==0)
-					a[i][d] =  k[0]*(r[i+1][d]-r[i][d]-l[0])/mass[0];
-				//else
-					//a[i][d] = -k[0]*(r[i][d]-r[i-1][d]-l[0])/mass[0];	
+				xi[s][d] = (r[s][d]-r[s+1][d]);	// temporal data, delta r
+				norm_xi += xi[s][d]*xi[s][d]; 	// quadratic sum
+			}
+			norm_xi=sqrt(norm_xi);
+
+			for (int d=0;d<DIM;d++){		
+				xi[s][d] = xi[s][d] * (1-l[s]/norm_xi);		// real deformation values
+			}
+			// finally the spring deformation is xi[s][d] = (r[s][d]-r[s+1][d])*(1-l[s]/norm_xi);
+		}
+		// ---> END spring deformation
+		
+		printf("MODEL\t%d\n",n);		// Time step for the pdb file
+		for(int i=0;i<N;i++){
+			for (int d=0;d<DIM;d++){
+				// aceleration (force) over the particle by the SPRINGS
+				if(i==0)
+					a[i][d] = (-xi[i][d]) * (k[i]/mass[i]);
+				else if (i==N)
+					a[i][d] = (xi[i-1][d])* (k[i-1]/mass[i]);
+				else
+					a[i][d] = (-xi[i][d]) * (k[i]/mass[i])+xi[i-1][d]*(k[i]/mass[i]);
+					
 				// generate the new velocity
 				v[i][d] += a[i][d] * dt;
 				// actualize the position
 				r[i][d] += v[i][d] * dt;  // where v[i] is v_(t+1/2 * dt)
-				cout << r[i][d] << "\t";
 			}
-			if(i==0)	
-				cout<<"1.00\t0.43\tC"<<endl;
-			else
-				cout<<"1.00\t0.41\tC"<<endl;
+			print_pdb_line(i+1,r[i][0],r[i][1],r[i][2]);
 		}
-		cout<<"ENDMDL"<<endl;
+		printf("ENDMDL\n");
 		t_i  += dt;
 		n++;
 	}
 	//---> END _Integrator leapfrog_
-	print_pdb_atom_line();
 }
